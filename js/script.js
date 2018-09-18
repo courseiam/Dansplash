96e9994543558c27d13adab7bff2cd9483436d5b
courseiam <enyapnad@hotmail.com>
2018-09-18 20:46:14 +0100

    Ajax call on scroll

--- js/script.js
+++ js/script.js
@@ -1,34 +1,57 @@
 const grid = document.querySelector('.grid');
 const htmlArray = []
-const page = 1
+const state = {
+    page: 1,
+} 
 
 
 
-const getImages = axios.get('https://api.unsplash.com/photos/', {
-    params: {
-        client_id: '4eb7b0a2d424f23026aff0be23ee0f36290dcfe9dee54bc69204a55f134a2a13',
-        per_page: '10',
-        page
-    }
-}).then( (response) => {
-    response.data.forEach(d => {
-        console.log(d)
-        htmlArray.push(`<div class="grid-item"><img src="${d.urls.regular}"></img></div>`)
-    });
-}).then(() => {
-    //console.log(htmlArray)
-    const htmlOut = htmlArray.join('\n');
-    grid.insertAdjacentHTML('beforeend', htmlOut)
-    
-}).then(() => {
-    const msnry = new Masonry( grid, {
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer',
-        percentPosition: true
-    });
+const getImages = ((page) => {
+    axios.get('https://api.unsplash.com/photos/', {
+        params: {
+            client_id: '4eb7b0a2d424f23026aff0be23ee0f36290dcfe9dee54bc69204a55f134a2a13',
+            per_page: '10',
+            page
+        }
+    }).then( (response) => {
+        //console.log(response.data)
+        response.data.forEach(d => {
+            // console.log(d)
+            //htmlArray = []
+            // htmlArray.push(`<div class="grid-item"><img src="${d.urls.regular}"></img></div>`)
+            grid.insertAdjacentHTML('beforeend', `<div class="grid-item"><img src="${d.urls.regular}"></img></div>`)
+        });
+    }).then(() => {
+        // grid.insertAdjacentHTML('beforeend', state.output)
+        // state.output = ""`
+    }).then(() => {
+        const msnry = new Masonry( grid, {
+            itemSelector: '.grid-item',
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+        });
 
-    imagesLoaded( grid ).on( 'progress', function() {
-        // layout Masonry after each image loads
-        msnry.layout();
-    });
+        imagesLoaded( grid ).on( 'progress', function() {
+            // layout Masonry after each image loads
+            msnry.layout();
+        });
+    })
 })
+
+const yHandler = () =>{
+	
+	const contentHeight = grid.offsetHeight;
+	const yOffset = window.pageYOffset; 
+	const scroll = window.scrollY; 
+	const y = yOffset + window.innerHeight;
+	//const y = scroll + window.innerHeight;
+	if(y >= contentHeight){
+        state.page++
+        getImages(state.page)
+        console.log('Fired!')
+    }
+}
+
+window.onscroll = yHandler;
+
+window.addEventListener('load', getImages(state.page));
